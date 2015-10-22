928493175abb5a25f0520dc135cfeba0f393d81f
Rui <rui.alonso@gigigo.com>
2015-10-22 12:57:11 +0200

    added ip address

--- server/base/serverUtils.js
+++ server/base/serverUtils.js
@@ -1,6 +1,19 @@
+/*
+This software is licensed under the MIT License.
+
+Copyright Fedor Indutny, 2012.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 var express = require('express');
 var bodyParser = require("body-parser");
 var fileSystemUtils = require('../filesystem.js');
+var ip = require("ip");
 
 var app = express();
 
@@ -10,6 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 
 var availableEndpoints = new Array();
+
+function _getIpAddress() {
+	var ipAddress = ip.address();
+	return ipAddress;
+}
 
 function _printServerHelp() {
 	console.log("*** Print server help");
@@ -145,7 +163,7 @@ exports.launchServer = function(serverName) {
 	_showServerHelp();
 
 	//launch server
-	var server = app.listen(3000, 'localhost', function () {
+	var server = app.listen(3000, _getIpAddress(), function () {
 		var host = server.address().address;
 		var port = server.address().port;
 
